55803d2ccc28bdd93076b941054bb56a5c772f20
zhuyu <zhuyu07123@qq.com>
2016-07-11 17:18:19 +0800

    edit layout

--- protected/modules/se_admin/views/layouts/main.php
+++ protected/modules/se_admin/views/layouts/main.php
@@ -244,7 +244,7 @@ Yii::app ()->clientScript->registerScriptFile ( Yii::app ()->theme->baseUrl . "/
                         <a href="<?php echo $this->createUrl('messages/list');?>"><i class="fa fa-edit"></i> 公告列表</a>
 		            </li>
                     <li class="treeview">
-                        <a href="<?php echo $this->createUrl('user/EditInfo');?>"><i class="fa fa-edit"></i> 客服管理</a>
+                        <a href="<?php echo $this->createUrl('customerservice/list');?>"><i class="fa fa-edit"></i> 客服管理</a>
                     </li>
                     <li class="treeview">
                         <a href="<?php echo $this->createUrl('user/register');?>"><i class="fa fa-edit"></i> 新增管理员</a>
